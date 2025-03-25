8a25a2ece064b80201263a997bed0a0c2855230f
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-03-25 17:52:37 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -6,4 +6,44 @@ public class PracticeProblem {
 
 	
 
+
+	public static void selectionSortName(String[] names, int[] ages){
+
+		for (int i = 0; i < names.length - 1; i++){
+			int smallestIndex = i;
+			for (int j = i + 1; j < names.length; j++){
+				if (names[j].compareToIgnoreCase(names[smallestIndex]) < 0){
+					smallestIndex = j;
+				}
+			}
+			String temp = names[i];
+			names[i] = names[smallestIndex];
+			names[smallestIndex] = temp;
+			int tempAge = ages[i];
+			ages[i] = ages[smallestIndex];
+			ages[smallestIndex] = tempAge;
+		}
+	}
+
+
+	public static void selectionSortAge(String[] names, int[] ages){
+		for (int i = 0; i < ages.length; i++){
+			int smallestIndex = i;
+			for (int j = i + 1; j < ages.length; j++){
+				if (ages[j] < ages[smallestIndex]){
+					smallestIndex = j;
+				}
+			}
+			int temp = ages[i];
+			ages[i] = ages[smallestIndex];
+			ages[smallestIndex] = temp;
+			String tempName = names[i];
+			names[i] = names[smallestIndex];
+			names[smallestIndex] = tempName;
+		}
+	}
+
+
+
+
 }
